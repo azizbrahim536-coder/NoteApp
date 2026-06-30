374d80e491686ab174b5fe1ca37c0fb3d134dc46
Aziz <azizbrahim536@gmail.com>
2026-06-30 15:19:15 +0100

    modifier controller

--- Notes.app/src/main/java/com/aziz/Notes/app/Controller/NoteController.java
+++ Notes.app/src/main/java/com/aziz/Notes/app/Controller/NoteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/notes")
+@RequestMapping("/notes")
 @CrossOrigin(origins = "http://localhost:4200")
 public class NoteController {
 
